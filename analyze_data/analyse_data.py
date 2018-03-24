7e47979abd16b79230d49ae3dc5166ff1b804849
PaulGarnierMuller <paulo19@live.fr>
2018-03-24 19:05:32 +0100

    tags sec

--- analyze_data/analyse_data.py
+++ analyze_data/analyse_data.py
@@ -16,10 +16,12 @@ tags_sec=["asiatique","france","anglo-saxon","hispanique","germanique","africain
     "baroque","impressionniste","moderne","photographie"]
 
 
-tags_related=[["rue"],["autre"],["chant","chanson","variete"],["cirque","magie"],["danse"],["humour","cafe"],["classique","opera"],
+tags_related_p=[["rue"],["autre"],["chant","chanson","variete"],["cirque","magie"],["danse"],["humour","cafe"],["classique","opera"],
     ["monde","musique"],["hip","hop","rnb","soul","musique"],["jazz","blues","reggae","musique"],["pop","rock","electro"],
     ["pluridisciplinaire"],["spectacle" ,"jeune"],["spectacle","musique","cabaret","operette"],["theatre"],
     ["cinema"],["livre"],["musee"],["exposition"],["conference"],["visite"]]
+
+tags_related_s=[]
 
 
 
@@ -39,15 +41,18 @@ def normalize_offer(n):
 
     return words_clean
 
-def analyze_offer(n):
+def analyze_offer_princ(n):
     words=normalize_offer(n)
     pref=[0]*len(tags)
     for i in range(len(tags)):
         for w in words:
-            pref[i]+=tags_related[i].count(w)
+            pref[i]+=tags_related_p[i].count(w)
     return pref
 
-    
-
-
-                
+def analyze_offer_sec(n):
+    words=normalize_offer(n)
+    pref=[0]*len(tags)
+    for i in range(len(tags)):
+        for w in words:
+            pref[i]+=tags_related_s[i].count(w)
+    return pref
